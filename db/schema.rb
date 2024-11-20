d1515aef51d9cfc76edba7c698cccdab220be500
Cristiano Craco <cracus89@gmail.com>
2024-11-20 10:38:19 +0100

    descriptio added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_19_125917) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_200527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_19_125917) do
     t.decimal "cost_per_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
