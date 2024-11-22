2dd2176891932f8bf138afb2231fee0fbf517da3
rodrigo bouabida <rdrgbouabida@gmail.com>
2024-11-22 16:03:24 +0100

    fix again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     dashboard_path # your path
   end
 
-
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username image])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username image])
   end
 end
